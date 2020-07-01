c641087d5e050a9c9ca0def8e33db14f6b5373ad
name <lqp336699@gmail.com>
2020-07-01 17:34:02 +0800

    00

--- front/vue.config.js
+++ front/vue.config.js
@@ -2,7 +2,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-    publicPath:'./wyhc/',
+    publicPath:'/wyhc/',
     chainWebpack: config => {
         config.plugin('provide').use(webpack.ProvidePlugin, [{
             $: 'jquery',
